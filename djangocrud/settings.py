9ced80d1bc1aae91cf846b765277a0261138d29f
ChapaSysAdmin <72357747+chapa0723@users.noreply.github.com>
2026-07-13 17:40:21 -0300

    Update ALLOWED_HOSTS to allow all hosts

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = False
 #DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'task.yacaresoft.com', '0.0.0.0']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['127.0.0.1', 'task.yacaresoft.com', '0.0.0.0']
+ALLOWED_HOSTS = ["*"]
 #RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 #if RENDER_EXTERNAL_HOSTNAME:
 #    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
